8d16ac73eae21cb0b6d1c358edd62753deddbfa5
German Kravtsov <grk55rus@mail.ru>
2023-10-12 17:54:20 +0600

    add authorization with JWT

--- index.js
+++ index.js
@@ -2,13 +2,41 @@ const express = require('express')
 const cors = require('cors')
 const httpProxy = require('http-proxy')
 const proxy = httpProxy.createProxyServer()
-const {sequelize} = require('sequelize')
+//const {sequelize} = require('sequelize')
 const app = express()
-app.use(cors())
+//app.use(cors())
 
-const db = require("./sequelize/config/config_to_connect.js")
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/getData", (req, res) => {
+const db = require("./app/models");
+const Role = db.role;
+
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync Db');
+  initial();
+});
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to bezkoder application." });
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
+
+
+
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
+/*app.get("/", (req, res) => {
     res.send("Hello from backend")
 })
 
@@ -29,5 +57,22 @@ const initApp = async () => {
         console.error("Unable to connect to the database:", error.original)
     }
 }
+initApp()*/
 
-initApp()
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+ 
+  Role.create({
+    id: 2,
+    name: "moderator"
+  });
+ 
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
+
